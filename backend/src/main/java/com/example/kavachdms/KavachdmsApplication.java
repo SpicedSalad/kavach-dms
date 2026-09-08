515f3e85938cbf625358c8726aee4a5ceff5688e
EARTH <earth.agarwal.6@gmail.com>
2026-09-08 13:08:32 +0530

    SpringBoot folders added

---
+++ backend/src/main/java/com/example/kavachdms/KavachdmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kavachdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavachdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavachdmsApplication.class, args);
+	}
+
+}
