055ca330defeea1409e50334b2c2059b059f2ce4
Satvik Akshintala <satvik.akshintala@gmail.com>
2026-09-11 00:09:50 +0530

    Prithvi Req 1

--- backend/src/main/java/com/example/kavachdms/repository/CaseMemberRepository.java
+++ backend/src/main/java/com/example/kavachdms/repository/CaseMemberRepository.java
@@ -3,6 +3,12 @@ package com.example.kavachdms.repository;
 import com.example.kavachdms.entity.CaseMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CaseMemberRepository extends JpaRepository<CaseMember, Long> {
 
+    Optional<CaseMember> findByCaseEntity_CaseIdAndUser_Email(
+            Long caseId,
+            String email
+    );
 }
